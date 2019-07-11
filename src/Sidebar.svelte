d6cc0f9f68092cf7c2e4a7206e86e1c98dde1db7
Silvan Buedenbender <silvanCodes@gmail.com>
2019-07-11 16:32:40 +0200

    Remove gap by default as it is not working correctly across devices

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -1,20 +1,20 @@
 <script>
     import { onMount } from 'svelte';
 
-    export let scale = '--s0';
+    export let gap = null;
+
+    $: id = gap
 
 	onMount(() => {
-        document.querySelectorAll(`.with-sidebar${scale} > *`).forEach(e => e.style.margin = `calc(-1 * var(${scale}) / 2)`);
-        document.querySelectorAll(`.with-sidebar${scale} > * > *`).forEach(e => e.style.margin = `calc(var(${scale}) / 2)`);
-        document.querySelector(`.not-sidebar${scale}`).style.minWidth = `calc(50% - var(${scale}))`;
+        if (gap) {
+            document.querySelectorAll(`.with-sidebar${id} > *`).forEach(e => e.style.margin = `calc(-1 * var(${gap}) / 2)`);
+            document.querySelectorAll(`.with-sidebar${id} > * > *`).forEach(e => e.style.margin = `calc(var(${gap}) / 2)`);
+            document.querySelector(`.with-sidebar${id} > * > .not-sidebar`).style.minWidth = `calc(50% - var(${gap}))`;
+        }
 	});
 </script>
 
 <style>
-    /* [class^="with-sidebar"] {
-        overflow: hidden;
-    } */
-
     [class^="with-sidebar"] > * {
         display: flex;
         flex-wrap: wrap;
@@ -28,16 +28,16 @@
         /* ↓ grow from nothing */
         flex-basis: 0;
         flex-grow: 999;
-        overflow: initial;
+        min-width: 50%;
     }
 </style>
 
-<div class={`with-sidebar${scale}`}>
+<div class={`with-sidebar${id}`}>
     <div>
         <div class="sidebar" >
             <slot name="sidebar"></slot>
         </div>
-        <div class={`not-sidebar${scale}`}>
+        <div class="not-sidebar">
             <slot name="not-sidebar"></slot>
         </div>
     </div>
