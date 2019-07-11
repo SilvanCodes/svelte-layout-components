8925da0fe58448bfc7696874a3294d38472588d8
Silvan Büdenbender <s.buedenbender@gipfelstuermer-kommunikation.com>
2019-07-11 17:29:09 +0200

    Wrap links in a-tags

--- src/staticContent.js
+++ src/staticContent.js
@@ -1,7 +1,7 @@
 const statements = [
     "This website is an experiment to learn webdesign.",
-    "It is layouted according to the ideas of https://every-layout.dev/",
-    "The color schema comes from https://colorsupplyyy.com/app",
+    `It is layouted according to the ideas of <a href="https://every-layout.dev/" target="blank">https://every-layout.dev/</a>.`,
+    `The color schema comes from <a href="https://colorsupplyyy.com/app" target="blank">https://colorsupplyyy.com/app</a>.`,
     "Press spacebar or swipe to cycle some layouts.",
     "More content to come."
 ];
