f89ce843c288464762708506a881c0acc14872ab
Silvan Buedenbender <silvanCodes@gmail.com>
2019-09-16 19:04:22 +0200

    Create Cluster component according to reference

--- src/SampleConfiguration_005.svelte
+++ src/SampleConfiguration_005.svelte
@@ -1,12 +1,27 @@
 <script>
 	import Cover from './layout/Cover.svelte';
 	import Bracket from './layout/Bracket.svelte';
+	import Cluster from './layout/Cluster.svelte';
 </script>
 
 <Cover>
 	<div slot="center">
 		<Bracket intrinsic={true}>
-			<p slot="center">center</p>
+			<div slot="center">
+				<p>WIDE CONTENT</p>
+				<Cluster>
+					<ul>
+						<li>A</li>
+						<li>B</li>
+						<li>C</li>
+						<li>D</li>
+						<li>E</li>
+						<li>F</li>
+						<li>G</li>
+						<li>H</li>
+					</ul>
+				</Cluster>
+			</div>
 		</Bracket>
 	</div>
 </Cover>
---
+++ src/layout/Cluster.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let justify = 'center';
+    export let align = 'center';
+    export let space = '--s0';
+
+    const id = space;
+
+    onMount(() => {
+        document.querySelectorAll(`.cluster${id} > *`).forEach(e => {
+            e.style.justifyContent = justify;
+            e.style.alignItems = align;
+            e.style.margin = `calc(var(${space}) / 2 * -1)`
+        });
+        document.querySelectorAll(`.cluster${id} > * > *`).forEach(e => e.style.margin = `calc(var(${space}) / 2)`);
+	});
+</script>
+
+<style>
+    [class^="cluster"] {
+        overflow: hidden;
+    }
+    :global([class^="cluster"] > *) {
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
+
+<div class={`cluster${id}`}>
+    <slot>
+    </slot>
+</div>
+
