15c714a1ba93e337cc81f56fdc1d6bfd6faa4c4b
Silvan Buedenbender <silvanCodes@gmail.com>
2019-09-17 11:11:35 +0200

    Create Grid component according to reference

---
+++ src/layout/Grid.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let min = '12rem';
+    export let space = '--s0';
+
+    const id = min + space;
+
+	onMount(() => {
+        document.querySelectorAll(`.grid${id}`).forEach(e => {
+            e.style.gridGap = `var(${space})`;
+            e.style.gridTemplateColumns = `repeat(auto-fit, minmax(${min}, 1fr))`;
+        });
+	});
+</script>
+
+<style>
+    [class^="grid"] {
+        display: grid;
+    }
+</style>
+
+<div class={`grid${id}`}>
+    <slot></slot>
+</div>
