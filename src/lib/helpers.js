1e251d8a3ce0da27fdcbe8a7680214ad0b684b00
silvan <SilvanCodes>
2019-09-20 11:46:56 +0200

    Refactor and add helper function(s)

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -2,11 +2,37 @@
  * Function to turn props of components into css variables or values.
  */
 function cssValue(...values) {
-    return values.map(v => `var(--${v.replace(/[^a-zA-Z0-9-_]/g, '')}, ${v})`).join(' ');
+    return values.map(v => `var(--${sanitize(v)}, ${v})`).join(' ');
 }
 
+/**
+ * Function to generate 'sanitized' Id.
+ */
 function buildId(...values) {
-    return values.map(v => typeof v === 'string' ? v.replace(/[^a-zA-Z0-9-_]/g, '_') : String(v)).join('-');
+    return values
+        .map(v => typeof v === 'string' ? sanitize(v, '_') : String(v))
+        .join('-');
 }
 
-export { cssValue, buildId }
+/**
+ * Function to get px value of variable or relative unit on DOM node.
+ */
+function pxValue(node, value) {
+    const style = node.style;
+    const currentValue = style.width;
+
+    style.width = cssValue(value);
+    const pixels = getComputedStyle(node).width;
+    style.width = currentValue;
+
+    return pixels.replace('px', '');
+}
+
+/**
+ * Function to make string CSS variable- and class-name compatible.
+ */
+function sanitize(string, replacement = '') {
+    return string.replace(/[^a-zA-Z0-9-_]/g, replacement);
+}
+
+export { cssValue, buildId, pxValue }
